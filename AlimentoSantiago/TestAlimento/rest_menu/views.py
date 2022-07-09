048c83eba4dde4965d7779a0457163f71b0c1fa6
LeanickOjeda <101678421+LeanickOjeda@users.noreply.github.com>
2022-07-09 10:53:23 -0400

    no se

--- AlimentoSantiago/TestAlimento/rest_menu/views.py
+++ AlimentoSantiago/TestAlimento/rest_menu/views.py
@@ -5,6 +5,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
+from core.forms import MenuForm
 from core.models import MenuSemana
 from .serializers import MenuSerializer
 @csrf_exempt
@@ -18,11 +19,10 @@ def lista_menu(request):
         serializer = MenuSerializer(menu, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = MenuSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        form = MenuForm(request.POST)
+        if form.is_valid():
+            form.save()
+
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
